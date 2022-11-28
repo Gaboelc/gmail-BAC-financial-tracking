cf3a0b3e99dbaf0f496ea8a9c52251c9cbfc2f0d
Gaboelc <gabrielleon917@gmail.com>
2022-11-28 05:09:08 -0600

    data cleared from get_emails function

--- gmail_imbox_checker.py
+++ gmail_imbox_checker.py
@@ -3,7 +3,7 @@ import html2text
 
 class gmail_checker:
     
-    def get_emails(self, email_user, password_user):
+    def get_emails(self, email_user, password_user, mark_seen=False):
         self.email_user = email_user
         self.password = password_user
         
@@ -18,22 +18,27 @@ class gmail_checker:
             h.ignore_links = True
             h.ignore_images = True
             
-            self.all_inbox_messages = imbox.messages()
+            self.all_inbox_messages = imbox.messages(unread=True)
+            print(self.all_inbox_messages)
             
             self.emails = list()
             for uid, message in self.all_inbox_messages:
-                
                 if message.sent_from[0]['email'] == 'notificacion@notificacionesbaccr.com':            
                     self.output = (h.handle(f'''{message.body}''').replace("\\r\\n", ""))
                     self.output = self.output.replace("\n", "")
                     self.mail_body = self.output[2:-2]
                     
                     self.email = {
+                        'id': uid,
 						'sent_from': message.sent_from[0]['email'],
 						'subject': message.subject,
 						'Message': self.mail_body
 					}
                     
                     self.emails.append(self.email)
+                    
+                    if mark_seen == True:
+                        imbox.mark_seen(uid)
+                        print('All reviewed emails were put on seen')
             
         return self.emails
